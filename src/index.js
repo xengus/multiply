f74cfa18af3f1f6f6a3dcef063191d978a26033d
xengus <serjiox13@gmail.com>
2019-10-15 22:35:11 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  a = first.split('');
+  b = second.split('');
+  let c = Array.from({ length: a.length + b.length - 1 }, (v) => 0);
+  for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < b.length; j++) {
+          c[i + j] += a[i] * b[j];
+      }
+  }
+  for (let i = c.length - 1; i > 0; i--) {
+    c[i-1]+=(c[i]/10|0);
+    c[i]=c[i]%10;
+  }
+  while(c[0]==0) c.shift();
+  return c.join('') || '0'; 
 }
